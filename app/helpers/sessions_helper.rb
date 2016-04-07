5db9fc4d146f2264061416ecf33a1ebda4b1f82e
RabitKing <wazaqpaul@gmail.com>
2016-04-07 18:01:44 +0000

    Made some changes to the helpers directory

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+    # logs in the user
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+    
+    # Returns the current logged in user if any?
+    def current_user
+        @current_user  ||= User.find_by(id: session[:user_id])
+    end
+    
+    # Returns true if the User not logged in 
+    def logged_in?
+        !current_user.nil?
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ActiveRecord::Base
     
     has_secure_password  
     validates :password, length:{minimum: 6}
+    
+    def User.digest(string)
+        # returns the hash digest of a given string
+        cost = ActiveModel::SecurePassword.min_cost ?
+        BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
